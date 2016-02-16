7bc04f026f15b1a880b3e04cf43b0d5c449a35f7
Po Chen <chenpaul914@gmail.com>
2016-02-17 02:43:54 +0800

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule Stripex.Mixfile do
     [app: :stripex,
      version: "0.1.0",
      elixir: "~> 1.2",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -17,5 +19,18 @@ defmodule Stripex.Mixfile do
   defp deps do
     [{:httpoison, "~> 0.8.0"},
      {:poison, "~> 2.1.0"}]
+  end
+
+  defp description do
+    '''
+    Stripe API wrapper
+    '''
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     maintainers: ["Po Chen"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/princemaple/stripex"}]
   end
 end
