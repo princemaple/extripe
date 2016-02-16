1dfaa95c75d07c35871423a96f669c2b89e0a18b
Po Chen <chenpaul914@gmail.com>
2016-02-16 23:02:29 +0800

    maybe struct is not a good idea here
    it might be a good idea with an extra layer of abstraction
    but I'm going to take it out for now

--- lib/stripex/api.ex
+++ lib/stripex/api.ex
@@ -13,7 +13,6 @@ defmodule Stripex.API do
     import Stripex.Utils, only: [normalize_params: 1]
 
     body
-    |> Map.from_struct
     |> normalize_params
     |> URI.encode_query
   end
@@ -26,7 +25,6 @@ defmodule Stripex.API do
     json
     |> Poison.decode!
     |> fetch_data
-    |> to_struct(module)
   end
 
   defp fetch_data(map) do
@@ -34,17 +32,5 @@ defmodule Stripex.API do
       {:ok, data} -> data
       :error -> map
     end
-  end
-
-  defp to_struct(maps, module) when is_list(maps) do
-    Enum.map(maps, &to_struct(&1, module))
-  end
-
-  defp to_struct(map, module) do
-    struct(module, atomize_keys(map))
-  end
-
-  defp atomize_keys(map) do
-    Enum.map(map, fn {k, v} -> {String.to_atom(k), v} end)
   end
 end
