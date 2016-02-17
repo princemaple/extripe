997181a675a46bc9960f56b0af749d3592cd1321
Po Chen <chenpaul914@gmail.com>
2016-02-18 02:33:57 +0800

    revise refund

--- lib/extripe/refund.ex
+++ lib/extripe/refund.ex
@@ -1,3 +1,5 @@
 defmodule Extripe.Refund do
-  use Extripe.Actions.CRUD, except: [:delete], resource: "refunds"
+  use Extripe.Actions.CRUD, except: [:create, :delete], resource: "refunds"
+
+  # TODO #create is scoped
 end
