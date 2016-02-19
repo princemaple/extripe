1971e5353a96b73d3d146176cd5afffde934d79f
Po Chen <chenpaul914@gmail.com>
2016-02-19 18:25:41 +0800

    implement singular resource
    e.g. /v1/balance
    a resource that has #show but does not require an id,
    consequently, an index action would work the same but is not
    semantically correct.

--- lib/extripe/actions/show.ex
+++ lib/extripe/actions/show.ex
@@ -4,17 +4,35 @@ defmodule Extripe.Actions.Show do
 
   defmacro __using__(opts) do
     {scope, opts} = Keyword.pop(opts, :scope)
-    {resource, _opts} = Keyword.pop(opts, :resource)
+    {resource, opts} = Keyword.pop(opts, :resource)
+    {singular, _opts} = Keyword.pop(opts, :singular)
+
+    has_scope_id = cond do
+      is_binary(scope) -> true
+      is_tuple(scope) or is_nil(scope) -> false
+    end
 
     code = cond do
-      is_tuple(scope) or is_nil(scope) ->
+      singular && !has_scope_id ->
+        quote do
+          def show do
+            API.get(Endpoint.build(unquote(scope), nil, unquote(resource)))
+          end
+        end
+      singular && has_scope_id ->
+        quote do
+          def show(scope_id) do
+            API.get(Endpoint.build(unquote(scope), scope_id, unquote(resource)))
+          end
+        end
+      !has_scope_id ->
         quote do
           def show(id) do
             API.get(Endpoint.build(unquote(scope), nil, unquote(resource), id))
           end
           defdelegate fetch(id), to: __MODULE__, as: :show
         end
-      is_binary(scope) ->
+       has_scope_id ->
         quote do
           def show(scope_id, id) do
             API.get(Endpoint.build(unquote(scope), scope_id, unquote(resource), id))
