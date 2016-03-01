ae9127cf46f4468a9e7f1351caa189fd93fe65da
Po Chen <chenpaul914@gmail.com>
2016-03-01 13:08:47 +0800

    refund#create should work with normal CRUD

--- lib/extripe/refund.ex
+++ lib/extripe/refund.ex
@@ -1,5 +1,3 @@
 defmodule Extripe.Refund do
-  use Extripe.Actions.CRUD, except: [:create, :delete], resource: "refunds"
-
-  # TODO #create is scoped
+  use Extripe.Actions.CRUD, except: [:delete], resource: "refunds"
 end
