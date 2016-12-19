72929541f9737d79f09d369a5b78e5691885b007
Po Chen <chenpaul914@gmail.com>
2016-12-20 09:00:58 +1100

    Update crud.ex

--- lib/extripe/actions/crud.ex
+++ lib/extripe/actions/crud.ex
@@ -1,16 +1,13 @@
 defmodule Extripe.Actions.CRUD do
-  import String, only: [capitalize: 1, to_atom: 1]
-
   @actions [:index, :show, :create, :update, :delete]
   @action_implementations Enum.map(@actions, fn action ->
-    {action, to_atom "Elixir.Extripe.Actions.#{action |> to_string |> capitalize}"}
+    {action, Module.concat(Extripe.Actions, action |> to_string |> Macro.camelize)}
   end)
 
   defmacro __using__(opts) do
-    actions = Keyword.get(opts, :only, @actions)
-    actions = actions -- Keyword.get(opts, :except, [])
+    actions = Keyword.pop(opts, :only, @actions)
+    actions = actions -- Keyword.pop(opts, :except, [])
     resource = Keyword.fetch!(opts, :resource)
-    opts = Keyword.drop(opts, [:only, :except])
 
     quote do
       @moduledoc ~s"""
@@ -22,9 +19,9 @@ defmodule Extripe.Actions.CRUD do
     end
   end
 
-  defp compile(actions, opts) do
-    compile(actions, opts, [])
-  end
+  defp compile(actions, opts, acc \\ [])
+
+  defp compile([], _, acc), do: acc
 
   for action <- @actions do
     defp compile([unquote(action) = action | rest], opts, acc) do
@@ -32,9 +29,5 @@ defmodule Extripe.Actions.CRUD do
         use unquote(@action_implementations[action]), unquote(opts)
       end | acc])
     end
-  end
-
-  defp compile([], _, acc) do
-    acc
   end
 end
