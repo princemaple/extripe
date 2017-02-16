a999f1cd79694f8fbd5126b91c79e03cf385e4c4
Po Chen <chenpaul914@gmail.com>
2017-02-16 14:25:39 +1100

    v1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Extripe.Mixfile do
 
   def project do
     [app: :extripe,
-     version: "0.3.3",
+     version: "1.0.0",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
