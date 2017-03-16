d3dbce21660c975bfe16694dcc026ad5392fe08a
Po Chen <chenpaul914@gmail.com>
2017-03-16 23:05:48 +1100

    add pay to order

--- lib/extripe/order.ex
+++ lib/extripe/order.ex
@@ -1,5 +1,14 @@
 defmodule Extripe.Order do
   use Extripe.Actions.CRUD, resource: "orders"
 
-  # TODO #pay
+  alias Extripe.Utils.{API, Endpoint}
+
+  @spec pay(id :: binary, customer_or_source :: Keyword.t)
+    :: {:ok, map} | {:error, binary}
+  def pay(id, customer: customer) do
+    API.post(Endpoint.build(nil, nil, "orders", id) <> "/pay", %{customer: customer})
+  end
+  def pay(id, source: source) do
+    API.post(Endpoint.build(nil, nil, "orders", id) <> "/pay", %{source: source})
+  end
 end
