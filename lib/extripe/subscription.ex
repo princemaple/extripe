9ccf3b717c875ab67f96cbb5b9a2961e4382d68b
Po Chen <chenpaul914@gmail.com>
2017-04-19 11:24:31 +1000

    unscope subcription

--- lib/extripe/subscription.ex
+++ lib/extripe/subscription.ex
@@ -1,10 +1,3 @@
 defmodule Extripe.Subscription do
-  use Extripe.Actions.CRUD, except: [:delete], scope: "customers", resource: "subscriptions"
-
-  alias Extripe.Utils.{API, Endpoint}
-
-  @spec delete(binary) :: {:ok, map} | {:error, binary}
-  def delete(id) do
-    API.delete(Endpoint.build(nil, nil, "subscriptions", id))
-  end
+  use Extripe.Actions.CRUD, resource: "subscriptions"
 end
