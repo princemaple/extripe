a0a3494d88b711f8cc0d0f58c70d39b331da8ae2
Po Chen <chenpaul914@gmail.com>
2018-01-23 18:56:18 +1100

    bumps

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule Extripe.Mixfile do
   end
 
   defp deps do
-    [{:httpoison, "~> 0.11"},
+    [{:httpoison, "~> 1.0"},
      {:poison, "~> 2.0 or ~> 3.0"},
      {:ex_doc, "~> 0.13", only: :dev}]
   end
